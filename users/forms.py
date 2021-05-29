b23a1147e39790c793445a7639602ea314a17c0d
TaliPavlov <65496380+TaliPavlov@users.noreply.github.com>
2021-05-29 20:25:43 +0300

    check

--- users/forms.py
+++ users/forms.py
@@ -1,18 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Customer
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 
-DATA_PLAN_CHOICES =(
+DATA_PLAN_CHOICES = (
     ("1", "Ultimate  100$/m"),
     ("2", "Regular 50$/m"),
     ("3", "Free 0$/m"),
-    )
+)
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-
 
     class Meta:
         model = User
@@ -21,9 +21,14 @@ class UserRegisterForm(UserCreationForm):
 
 class ProfileForm(forms.ModelForm):
     credit_card = forms.IntegerField()
-    data_plan = forms.ChoiceField(choices = DATA_PLAN_CHOICES)
-
+    data_plan = forms.ChoiceField(choices=DATA_PLAN_CHOICES)
 
     class Meta:
         model = Profile
         fields = ['credit_card', 'data_plan']
+
+
+class CustomerCreateForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
